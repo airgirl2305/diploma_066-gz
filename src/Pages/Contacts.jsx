7ca597a0c315171a936bc42fda3aab2ca507d4f2
airgirl2305 <kheda.askhabova@gmail.com>
2024-05-27 03:38:08 +0300

    Пробудем микроразметку адреса компании

--- src/Pages/Contacts.jsx
+++ src/Pages/Contacts.jsx
@@ -4,13 +4,21 @@ export const Contacts = () => {
     <div className="contacts">
       <h1 className="contacts__page-title">Контакты</h1>
       <p className="page-text">
-        195030, РОССИЙСКАЯ ФЕДЕРАЦИЯ, ГОРОД САНКТ-ПЕТЕРБУРГ, УЛИЦА КОММУНЫ, ДОМ № 63, ЛИТЕРА А, ПОМ.
-        5Н, КОМ. 53.
+        <div itemScope itemType="http://schema.org/Organization">
+          <div itemProp="address" itemScope itemType="http://schema.org/PostalAddress">
+        <span itemProp="name">OOO "Гик-с"</span>
+        195030, РОССИЙСКАЯ ФЕДЕРАЦИЯ, ГОРОД <span itemProp="addressLocality">САНКТ-ПЕТЕРБУРГ</span>, <span
+            itemProp="streetAddress">УЛИЦА КОММУНЫ, ДОМ № 63, ЛИТЕРА А, ПОМ.
+        5Н, КОМ. 53.</span>
         <br />
-        ТЕЛЕФОН: +7(812)628-2017
+        ТЕЛЕФОН: <span itemProp="telephone">+7(812)628-2017</span>
         <br />
-        ЭЛЕКТРОННАЯ ПОЧТА:&nbsp;CONTACT@GEEK-S.COM
+            ЭЛЕКТРОННАЯ ПОЧТА:&nbsp;<span itemProp="email"></span>CONTACT@GEEK-S.COM
+        </div>
+        </div>
       </p>
     </div>
+
+
   );
 };
